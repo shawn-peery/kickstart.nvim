5ddcda9459f8fe9b3f1208f067a8faa451b9013f
Shawn Peery <shawn.w8465@gmail.com>
2025-05-13 14:58:44 -0600

    CHG: Trying. Not Working

--- init.lua
+++ init.lua
@@ -475,6 +475,105 @@ require('lazy').setup({
         { '<leader>t_', hidden = true },
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>w_', hidden = true },
+        -- Debugger
+        {
+          '<leader>d',
+          group = 'Debugger',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>dt',
+          function()
+            require('dap').toggle_breakpoint()
+          end,
+          desc = 'Toggle Breakpoint',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>dc',
+          function()
+            require('dap').continue()
+          end,
+          desc = 'Continue',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>di',
+          function()
+            require('dap').step_into()
+          end,
+          desc = 'Step Into',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>do',
+          function()
+            require('dap').step_over()
+          end,
+          desc = 'Step Over',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>du',
+          function()
+            require('dap').step_out()
+          end,
+          desc = 'Step Out',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>dr',
+          function()
+            require('dap').repl.open()
+          end,
+          desc = 'Open REPL',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>dl',
+          function()
+            require('dap').run_last()
+          end,
+          desc = 'Run Last',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>dq',
+          function()
+            require('dap').terminate()
+            require('dapui').close()
+            require('nvim-dap-virtual-text').toggle()
+          end,
+          desc = 'Terminate',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>db',
+          function()
+            require('dap').list_breakpoints()
+          end,
+          desc = 'List Breakpoints',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>de',
+          function()
+            require('dap').set_exception_breakpoints { 'all' }
+          end,
+          desc = 'Set Exception Breakpoints',
+          nowait = true,
+          remap = false,
+        },
       }
     end,
   },
@@ -1067,6 +1166,17 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'mfussenegger/nvim-dap',
+    event = 'VeryLazy',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+      'jay-babu/mason-nvim-dap.nvim',
+      'theHamsta/nvim-dap-virtual-text',
+    },
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
@@ -1124,5 +1234,106 @@ vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 --
 --
 --
+--
+-- DEBUGGERS
+local mason_dap = require 'mason-nvim-dap'
+local dap = require 'dap'
+local ui = require 'dapui'
+local dap_virtual_text = require 'nvim-dap-virtual-text'
+
+-- Dap Virtual Text
+dap_virtual_text.setup()
+
+mason_dap.setup {
+  ensure_installed = { 'cppdbg', 'js-debug-adapter' },
+  automatic_installation = true,
+  handlers = {
+    function(config)
+      require('mason-nvim-dap').default_setup(config)
+    end,
+  },
+}
+
+-- Configurations
+
+-- Define a shared config table
+local js_ts_config = {
+  {
+    type = 'pwa-chrome',
+    request = 'launch',
+    name = 'Test',
+    url = 'http://localhost:3000',
+    webRoot = '${workspaceFolder}',
+    userDataDir = '${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir',
+  },
+  {
+    type = 'pwa-node',
+    request = 'launch',
+    name = 'Launch file',
+    program = '${file}',
+    cwd = '${workspaceFolder}',
+  },
+  {
+    type = 'pwa-node',
+    request = 'attach',
+    name = 'Attach to process',
+    processId = require('dap.utils').pick_process,
+    cwd = '${workspaceFolder}',
+  },
+}
+
+-- Assign the same config to multiple filetypes
+for _, ft in ipairs { 'javascript', 'typescript', 'typescriptreact' } do
+  dap.configurations[ft] = js_ts_config
+end
+
+dap.configurations = {
+  c = {
+    {
+      name = 'Launch file',
+      type = 'cppdbg',
+      request = 'launch',
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+      cwd = '${workspaceFolder}',
+      stopAtEntry = false,
+      MIMode = 'lldb',
+    },
+    {
+      name = 'Attach to lldbserver :1234',
+      type = 'cppdbg',
+      request = 'launch',
+      MIMode = 'lldb',
+      miDebuggerServerAddress = 'localhost:1234',
+      miDebuggerPath = '/usr/bin/lldb',
+      cwd = '${workspaceFolder}',
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+    },
+  },
+}
+
+-- Dap UI
+
+ui.setup()
+
+vim.fn.sign_define('DapBreakpoint', { text = '🐞' })
+
+dap.listeners.before.attach.dapui_config = function()
+  ui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  ui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  ui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  ui.close()
+end
+
+-- Keymaps
 --
 --
