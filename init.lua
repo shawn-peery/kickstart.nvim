600a37ed7ea6ef47a5b1f77471d6d7aea86cd0a4
Shawn Peery <shawn.w8465@gmail.com>
2025-05-15 11:44:50 -0600

    CHG: Adding Debugger

--- init.lua
+++ init.lua
@@ -1066,6 +1066,34 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  {
+    'microsoft/vscode-js-debug',
+    lazy = true,
+    build = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out',
+  },
+  {
+    'mxsdev/nvim-dap-vscode-js',
+    dependencies = { 'mfussenegger/nvim-dap' },
+
+    config = function()
+      require('dap-vscode-js').setup {
+        -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+        debugger_path = 'C:\\Users\\Shawn\\AppData\\Local\\nvim-data\\lazy\\vscode-js-debug', -- Path to vscode-js-debug installation.
+        -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+        adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+        -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+        -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+        -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+      }
+      -- for _, language in ipairs { 'typescript', 'javascript' } do
+      --   require('dap').configurations[language] = {
+      --     ..., -- see below
+      --   }
+      -- end
+    end,
+
+    adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
