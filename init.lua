8235e3d79d3814c8e4c4f96f5045df529c18def5
Shawn Peery <shawn.w8465@gmail.com>
2026-07-28 13:58:18 -0600

    Merge pull request #26 from shawn-peery/feature/enable-spell-check
    feature(gitcommit): enable spell check

--- init.lua
+++ init.lua
@@ -1134,3 +1134,11 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.api.nvim_set_hl(0, 'gitcommitOverflow', { fg = '#ff5555', bold = true })
   end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'gitcommit',
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = 'en_us'
+  end,
+})
